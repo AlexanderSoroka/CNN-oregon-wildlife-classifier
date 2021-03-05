b4a9e7a40f8674f74f8f51b89895f87a806239b5
WHO777 <61012068+WHO777@users.noreply.github.com>
2021-03-05 18:41:51 +0300

    Update train.py

--- train.py
+++ train.py
@@ -58,11 +58,11 @@ def create_dataset(filenames, batch_size):
 
 def build_model():
   inputs = tf.keras.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
-  x = tf.keras.layers.Conv2D(filters=8, kernel_size=3, activation=tf.keras.layers.ReLU())(inputs)
+  x = tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation=tf.keras.layers.ReLU())(inputs)
   x = tf.keras.layers.MaxPool2D()(x)
-  x = tf.keras.layers.Conv2D(filters=16, kernel_size=3, activation=tf.keras.layers.ReLU())(x)
+  x = tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation=tf.keras.layers.ReLU())(x)
   x = tf.keras.layers.MaxPool2D()(x) 
-  x = tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation=tf.keras.layers.ReLU())(x)
+  x = tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation=tf.keras.layers.ReLU())(x)
   x = tf.keras.layers.MaxPool2D()(x)
   #x = tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation=tf.keras.layers.ReLU())(x)
   x = tf.keras.layers.Flatten()(x)
