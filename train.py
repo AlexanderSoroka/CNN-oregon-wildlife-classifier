5e06ffe2db2bdf1835d6331ff13f18616f07cf92
WHO777 <61012068+WHO777@users.noreply.github.com>
2021-03-07 15:40:39 +0300

    Update train.py

--- train.py
+++ train.py
@@ -78,7 +78,7 @@ def build_model():
   x = tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation=tf.keras.layers.ReLU())(x)
   x = tf.keras.layers.Flatten()(x)
   print('\n\n\n\n     ',x)
-  x = tf.keras.layers.Dense(40, activation=tf.keras.layers.ReLU())(x)
+  #x = tf.keras.layers.Dense(40, activation=tf.keras.layers.ReLU())(x)
   outputs = tf.keras.layers.Dense(NUM_CLASSES, activation=tf.keras.activations.softmax)(x)
   return tf.keras.Model(inputs=inputs, outputs=outputs)
 
